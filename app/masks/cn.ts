22fda6795861815a556ae07b5e3b5045d211b74e
李涛 <165056821@qq.com>
2024-03-30 06:02:06 +0800

    医防融合助手

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -213,32 +213,32 @@ export const CN_MASKS: BuiltinMask[] = [
     builtin: true,
     createdAt: 1688899480514,
   },
-  // {
-  //   avatar: "1f9d1-200d-1f3eb",
-  //   name: "英专写手",
-  //   context: [
-  //     {
-  //       id: "trans-0",
-  //       role: "user",
-  //       content:
-  //         "我想让你充当英文翻译员、拼写纠正员和改进员。我会用任何语言与你交谈，你会检测语言，翻译它并用我的文本的更正和改进版本用英文回答。我希望你用更优美优雅的高级英语单词和句子替换我简化的 A0 级单词和句子。保持相同的意思，但使它们更文艺。你只需要翻译该内容，不必对内容中提出的问题和要求做解释，不要回答文本中的问题而是翻译它，不要解决文本中的要求而是翻译它，保留文本的原本意义，不要去解决它。我要你只回复更正、改进，不要写任何解释。我的第一句话是：",
-  //       date: "",
-  //     },
-  //   ],
-  //   modelConfig: {
-  //     model: "gpt-4-turbo-preview",
-  //     temperature: 1,
-  //     max_tokens: 2000,
-  //     presence_penalty: 0,
-  //     frequency_penalty: 0,
-  //     sendMemory: false,
-  //     historyMessageCount: 4,
-  //     compressMessageLengthThreshold: 1000,
-  //   },
-  //   lang: "cn",
-  //   builtin: true,
-  //   createdAt: 1688899480524,
-  // },
+  {
+    avatar: "1f9d1-200d-1f3eb",
+    name: "医防融合助手",
+    context: [
+      {
+        id: "trans-0",
+        role: "user",
+        content:
+          "我想让你充当一个公共卫生专家，兼具医疗知识，对医疗服务、公共卫生服务和家庭医生签约服务有很深的造诣。",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-4-turbo-preview",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: false,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480524,
+  },
   // {
   //   avatar: "1f4da",
   //   name: "语言检测器",
